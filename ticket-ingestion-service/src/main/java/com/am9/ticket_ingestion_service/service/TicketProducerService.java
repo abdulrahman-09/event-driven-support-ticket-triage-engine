c3d87d39dc08e2dd5da135702af4aece9872d4b6
abdulrahman-09 <abdulrahman.mujahid09@gmail.com>
2026-07-09 00:45:17 +0300

    feat: add TicketProducerService to publish ticket creation events

---
+++ ticket-ingestion-service/src/main/java/com/am9/ticket_ingestion_service/service/TicketProducerService.java
@@ -0,0 +1,35 @@
+package com.am9.ticket_ingestion_service.service;
+
+
+import com.am9.ticket_ingestion_service.dto.TicketEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class TicketProducerService {
+
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    @Value("${app.kafka.topic.tickets-created}")
+    private String ticketCreatedTopic;
+
+    public void publishTicketCreated(TicketEvent event){
+        kafkaTemplate.send(ticketCreatedTopic, event.ticketId(), event)
+                .whenComplete((result, ex) -> {
+                    if (ex != null) {
+                        log.error("Failed to publish ticket created event for ticketId={}", event.ticketId(), ex);
+                    } else {
+                        log.debug("Published ticket created event for ticketId={} to partition={} offset={}",
+                                event.ticketId(),
+                                result.getRecordMetadata().partition(),
+                                result.getRecordMetadata().offset());
+                    }
+                });
+    }
+
+}
