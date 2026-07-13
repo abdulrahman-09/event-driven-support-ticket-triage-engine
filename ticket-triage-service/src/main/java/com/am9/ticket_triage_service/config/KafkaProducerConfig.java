71fdd4e87da3e0bf7cf9a98c0fb2a7fd4110078d
abdulrahman-09 <abdulrahman.mujahid09@gmail.com>
2026-07-14 02:20:55 +0300

    chore: setup configuration for Kafka topics and producer in ticket triage service

--- ticket-triage-service/src/main/java/com/am9/ticket_triage_service/config/KafkaProducerConfig.java
+++ ticket-triage-service/src/main/java/com/am9/ticket_triage_service/config/KafkaProducerConfig.java
@@ -1,5 +1,41 @@
 package com.am9.ticket_triage_service.config;
 
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
 public class KafkaProducerConfig {
-    // To Do
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServer;
+
+    @Bean
+    public ProducerFactory<String, Object> producerFactory(){
+
+        Map<String, Object> config = new HashMap<>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        config.put(ProducerConfig.ACKS_CONFIG, "all");
+        config.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true);
+
+
+        return new DefaultKafkaProducerFactory<>(config);
+
+    }
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate(ProducerFactory<String, Object> producerFactory){
+        return new KafkaTemplate<>(producerFactory);
+    }
 }
--- ticket-triage-service/src/main/java/com/am9/ticket_triage_service/config/KafkaTopicConfig.java
+++ ticket-triage-service/src/main/java/com/am9/ticket_triage_service/config/KafkaTopicConfig.java
@@ -1,5 +1,44 @@
 package com.am9.ticket_triage_service.config;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
 public class KafkaTopicConfig {
-    // To Do
+
+    @Value("${app.kafka.topic.tickets-critical}")
+    private String ticketsCritical;
+
+    @Value("${app.kafka.topic.tickets-medium}")
+    private String ticketsMedium;
+
+    @Value("${app.kafka.topic.tickets-low}")
+    private String ticketsLow;
+
+    @Value("${app.kafka.topic.tickets-dlq}")
+    private String ticketsDlq;
+
+
+    @Bean
+    public NewTopic ticketsCriticalTopic() {
+        return TopicBuilder.name(ticketsCritical).partitions(3).replicas(1).build();
+    }
+
+    @Bean
+    public NewTopic ticketsMediumTopic() {
+        return TopicBuilder.name(ticketsMedium).partitions(3).replicas(1).build();
+    }
+
+    @Bean
+    public NewTopic ticketsLowTopic() {
+        return TopicBuilder.name(ticketsLow).partitions(3).replicas(1).build();
+    }
+
+    @Bean
+    public NewTopic ticketsDlqTopic() {
+        return TopicBuilder.name(ticketsDlq).partitions(3).replicas(1).build();
+    }
 }
